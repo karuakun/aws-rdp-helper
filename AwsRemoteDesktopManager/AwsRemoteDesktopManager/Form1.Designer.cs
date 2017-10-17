51e644643d35703a8c3485b3a57f969121629bef
karuakun <karuakun@gmail.com>
2017-10-17 10:05:13 +0900

    AwsRemoteDesktopManagerを追加した

---
+++ AwsRemoteDesktopManager/AwsRemoteDesktopManager/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace AwsRemoteDesktopManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lstEc2Instances = new System.Windows.Forms.ListBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblPrivateIP = new System.Windows.Forms.Label();
+            this.btnLaunchRsc = new System.Windows.Forms.Button();
+            this.txtPrivateIpAddress = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtHowto = new System.Windows.Forms.TextBox();
+            this.lblHowto = new System.Windows.Forms.Label();
+            this.lnkHowto = new System.Windows.Forms.LinkLabel();
+            this.lblProfileName = new System.Windows.Forms.Label();
+            this.lblAccessKey = new System.Windows.Forms.Label();
+            this.lblRegion = new System.Windows.Forms.Label();
+            this.txtProfileName = new System.Windows.Forms.TextBox();
+            this.txtAccessKey = new System.Windows.Forms.TextBox();
+            this.txtRegion = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 81);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lstEc2Instances);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lblPassword);
+            this.splitContainer1.Panel2.Controls.Add(this.lblPrivateIP);
+            this.splitContainer1.Panel2.Controls.Add(this.btnLaunchRsc);
+            this.splitContainer1.Panel2.Controls.Add(this.txtPrivateIpAddress);
+            this.splitContainer1.Panel2.Controls.Add(this.txtPassword);
+            this.splitContainer1.Size = new System.Drawing.Size(447, 152);
+            this.splitContainer1.SplitterDistance = 218;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // lstEc2Instances
+            // 
+            this.lstEc2Instances.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstEc2Instances.FormattingEnabled = true;
+            this.lstEc2Instances.ItemHeight = 12;
+            this.lstEc2Instances.Location = new System.Drawing.Point(0, 0);
+            this.lstEc2Instances.Name = "lstEc2Instances";
+            this.lstEc2Instances.Size = new System.Drawing.Size(218, 152);
+            this.lstEc2Instances.TabIndex = 0;
+            this.lstEc2Instances.SelectedIndexChanged += new System.EventHandler(this.lstEc2Instances_SelectedIndexChanged);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(9, 37);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(54, 12);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblPrivateIP
+            // 
+            this.lblPrivateIP.AutoSize = true;
+            this.lblPrivateIP.Location = new System.Drawing.Point(10, 12);
+            this.lblPrivateIP.Name = "lblPrivateIP";
+            this.lblPrivateIP.Size = new System.Drawing.Size(51, 12);
+            this.lblPrivateIP.TabIndex = 2;
+            this.lblPrivateIP.Text = "PrivateIP";
+            // 
+            // btnLaunchRsc
+            // 
+            this.btnLaunchRsc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLaunchRsc.Location = new System.Drawing.Point(141, 59);
+            this.btnLaunchRsc.Name = "btnLaunchRsc";
+            this.btnLaunchRsc.Size = new System.Drawing.Size(75, 23);
+            this.btnLaunchRsc.TabIndex = 1;
+            this.btnLaunchRsc.Text = "実行";
+            this.btnLaunchRsc.UseVisualStyleBackColor = true;
+            this.btnLaunchRsc.Click += new System.EventHandler(this.btnLaunchRsc_Click);
+            // 
+            // txtPrivateIpAddress
+            // 
+            this.txtPrivateIpAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPrivateIpAddress.Location = new System.Drawing.Point(69, 9);
+            this.txtPrivateIpAddress.Name = "txtPrivateIpAddress";
+            this.txtPrivateIpAddress.ReadOnly = true;
+            this.txtPrivateIpAddress.Size = new System.Drawing.Size(147, 19);
+            this.txtPrivateIpAddress.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(69, 34);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.ReadOnly = true;
+            this.txtPassword.Size = new System.Drawing.Size(147, 19);
+            this.txtPassword.TabIndex = 0;
+            // 
+            // txtHowto
+            // 
+            this.txtHowto.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHowto.Location = new System.Drawing.Point(0, 260);
+            this.txtHowto.Multiline = true;
+            this.txtHowto.Name = "txtHowto";
+            this.txtHowto.ReadOnly = true;
+            this.txtHowto.Size = new System.Drawing.Size(447, 32);
+            this.txtHowto.TabIndex = 1;
+            this.txtHowto.Text = "Set-AWSCredentials –AccessKey {accessKey} –SecretKey {secretKey}\r\nSet-DefaultAWSR" +
+    "egion -Region ap-northeast-1";
+            // 
+            // lblHowto
+            // 
+            this.lblHowto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblHowto.AutoSize = true;
+            this.lblHowto.Location = new System.Drawing.Point(0, 245);
+            this.lblHowto.Name = "lblHowto";
+            this.lblHowto.Size = new System.Drawing.Size(197, 12);
+            this.lblHowto.TabIndex = 2;
+            this.lblHowto.Text = "Default Profile の初期化（PowerShell）";
+            // 
+            // lnkHowto
+            // 
+            this.lnkHowto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lnkHowto.AutoSize = true;
+            this.lnkHowto.Location = new System.Drawing.Point(0, 293);
+            this.lnkHowto.Name = "lnkHowto";
+            this.lnkHowto.Size = new System.Drawing.Size(191, 12);
+            this.lnkHowto.TabIndex = 3;
+            this.lnkHowto.TabStop = true;
+            this.lnkHowto.Text = "AWS Tools for Windows PowerShell ";
+            this.lnkHowto.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // lblProfileName
+            // 
+            this.lblProfileName.AutoSize = true;
+            this.lblProfileName.Location = new System.Drawing.Point(0, 9);
+            this.lblProfileName.Name = "lblProfileName";
+            this.lblProfileName.Size = new System.Drawing.Size(67, 12);
+            this.lblProfileName.TabIndex = 2;
+            this.lblProfileName.Text = "ProfileName";
+            // 
+            // lblAccessKey
+            // 
+            this.lblAccessKey.AutoSize = true;
+            this.lblAccessKey.Location = new System.Drawing.Point(0, 51);
+            this.lblAccessKey.Name = "lblAccessKey";
+            this.lblAccessKey.Size = new System.Drawing.Size(62, 12);
+            this.lblAccessKey.TabIndex = 2;
+            this.lblAccessKey.Text = "AccessKey";
+            // 
+            // lblRegion
+            // 
+            this.lblRegion.AutoSize = true;
+            this.lblRegion.Location = new System.Drawing.Point(0, 30);
+            this.lblRegion.Name = "lblRegion";
+            this.lblRegion.Size = new System.Drawing.Size(40, 12);
+            this.lblRegion.TabIndex = 2;
+            this.lblRegion.Text = "Region";
+            // 
+            // txtProfileName
+            // 
+            this.txtProfileName.Location = new System.Drawing.Point(73, 6);
+            this.txtProfileName.Name = "txtProfileName";
+            this.txtProfileName.ReadOnly = true;
+            this.txtProfileName.Size = new System.Drawing.Size(118, 19);
+            this.txtProfileName.TabIndex = 0;
+            // 
+            // txtAccessKey
+            // 
+            this.txtAccessKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAccessKey.Location = new System.Drawing.Point(72, 48);
+            this.txtAccessKey.Name = "txtAccessKey";
+            this.txtAccessKey.ReadOnly = true;
+            this.txtAccessKey.Size = new System.Drawing.Size(369, 19);
+            this.txtAccessKey.TabIndex = 0;
+            // 
+            // txtRegion
+            // 
+            this.txtRegion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRegion.Location = new System.Drawing.Point(73, 27);
+            this.txtRegion.Name = "txtRegion";
+            this.txtRegion.ReadOnly = true;
+            this.txtRegion.Size = new System.Drawing.Size(368, 19);
+            this.txtRegion.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 306);
+            this.Controls.Add(this.lblRegion);
+            this.Controls.Add(this.lblAccessKey);
+            this.Controls.Add(this.lnkHowto);
+            this.Controls.Add(this.txtRegion);
+            this.Controls.Add(this.txtAccessKey);
+            this.Controls.Add(this.txtProfileName);
+            this.Controls.Add(this.lblProfileName);
+            this.Controls.Add(this.lblHowto);
+            this.Controls.Add(this.txtHowto);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "RemoteDesktopManager";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox lstEc2Instances;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLaunchRsc;
+        private System.Windows.Forms.Label lblPrivateIP;
+        private System.Windows.Forms.TextBox txtPrivateIpAddress;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtHowto;
+        private System.Windows.Forms.Label lblHowto;
+        private System.Windows.Forms.LinkLabel lnkHowto;
+        private System.Windows.Forms.Label lblProfileName;
+        private System.Windows.Forms.Label lblAccessKey;
+        private System.Windows.Forms.Label lblRegion;
+        private System.Windows.Forms.TextBox txtProfileName;
+        private System.Windows.Forms.TextBox txtAccessKey;
+        private System.Windows.Forms.TextBox txtRegion;
+    }
+}
+
---
+++ AwsRemoteDesktopManager/AwsRemoteDesktopManager/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Diagnostics;
+
+using Amazon;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Amazon.Runtime.CredentialManagement;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AwsRemoteDesktopManager
+{
+    public partial class Form1 : Form
+    {
+        //public string RegionName { get; set; } = "ap-northeast-1";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Shown(object sender, EventArgs e)
+        {
+            SetCurrentProfile("Default");
+            await ShowEc2Instances();
+        }
+
+        private async void lstEc2Instances_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            await ShowEc2InstanceDetail();
+        }
+
+        private void btnLaunchRsc_Click(object sender, EventArgs e)
+        {
+            var privateIpAddress = txtPrivateIpAddress.Text;
+            Process.Start("mstsc.exe", $"/v:{privateIpAddress}");
+        }
+
+        private void SetCurrentProfile(string profileName)
+        {
+            var profile = GetAwsProfile(profileName);
+            if (null == profile)
+            {
+                return;
+            }
+            txtProfileName.Text = profile.Name;
+            txtAccessKey.Text = profile.Options.AccessKey;
+            txtRegion.Text = profile.Region.DisplayName;
+        }
+
+        public async Task<IEnumerable<Instance>> GetEc2Instances()
+        {
+            var client = new AmazonEC2Client();
+            var result = await client.DescribeInstancesAsync();
+            return result
+                ?.Reservations
+                ?.SelectMany(r => r.Instances);
+        }
+
+        private async Task ShowEc2Instances()
+        {
+            var instances = (await GetEc2Instances())
+                ?.Where(i => PlatformValues.Windows == i.Platform)
+                ?.Select(i => new { Instance = i, Display = $"{i.Tags.FirstOrDefault(t => t.Key == "Name")?.Value}" })
+                ?.ToList();
+            if (null == instances)
+            {
+                return;
+            }
+            lstEc2Instances.DisplayMember = "Display";
+            lstEc2Instances.ValueMember = "Instance";
+            lstEc2Instances.DataSource = instances;
+        }
+
+        public async Task ShowEc2InstanceDetail()
+        {
+            var instance = lstEc2Instances.SelectedValue as Instance;
+            if (null == instance)
+            {
+                return;
+            }
+            txtPassword.Text = await GetEc2Password(instance);
+            txtPrivateIpAddress.Text = instance.PrivateIpAddress;
+        }
+
+        //public CredentialProfile RegisterAwsProfile(string profileName, string accessKey, string secretKey)
+        //{
+        //    var profile = new CredentialProfile(profileName, new CredentialProfileOptions
+        //    {
+        //        AccessKey = accessKey,
+        //        SecretKey = secretKey,
+        //    });
+        //    profile.Region = RegionEndpoint.GetBySystemName(RegionName);
+
+        //    var credentialFile = new NetSDKCredentialsFile();
+        //    credentialFile.RegisterProfile(profile);
+
+        //    return profile;
+        //}
+        //public List<CredentialProfile> GetAwsProfiles()
+        //{
+        //    var credentialFile = new NetSDKCredentialsFile();
+        //    return credentialFile.ListProfiles();
+        //}
+
+        public CredentialProfile GetAwsProfile(string profileName)
+        {
+            var credentialFile = new NetSDKCredentialsFile();
+            CredentialProfile profile = null;
+            if (!credentialFile.TryGetProfile(profileName, out profile))
+            {
+                return null;
+            }
+            return profile;
+        }
+
+        public async Task<string> GetEc2Password(Instance instance)
+        {
+            var client = new AmazonEC2Client();
+            var result = await client.GetPasswordDataAsync(new GetPasswordDataRequest
+            {
+                InstanceId = instance.InstanceId
+            });
+            if (string.IsNullOrEmpty(result.PasswordData))
+            {
+                return string.Empty;
+            }
+            var keyLocation = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), @"Key\key.cer");
+            var key = File.ReadAllText(keyLocation);
+            return result.GetDecryptedPassword(key);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://aws.amazon.com/jp/powershell/");
+        }
+
+
+    }
+}
